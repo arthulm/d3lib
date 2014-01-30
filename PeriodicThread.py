13a5177687c7bb5f98ea686dd72fb61f2105cfb7
Patrick Welzel <patrick@kcirtap.de>
2014-01-30 04:51:41 +0100

    removed too much indenting in PeriodicThread._execute so the sleep gets now executed after each try

--- PeriodicThread.py
+++ PeriodicThread.py
@@ -1,4 +1,4 @@
-from .EThread import EThread
+from EThread import EThread
 import time
 
 class PeriodicThread(EThread):
@@ -10,7 +10,7 @@ class PeriodicThread(EThread):
     def _execute(self):
         if self._periodicFunction != None:
             self._periodicFunction()
-            time.sleep(self._sleep)
+        time.sleep(self._sleep)
 
 class PeriodicThread2(EThread):
     def __init__(self,sleeptime,func=None,outstream=None):
@@ -21,4 +21,4 @@ class PeriodicThread2(EThread):
     def _execute(self):
         if self._periodicFunction != None:
             self._periodicFunction(self)
-            time.sleep(self._sleep)
+        time.sleep(self._sleep)
